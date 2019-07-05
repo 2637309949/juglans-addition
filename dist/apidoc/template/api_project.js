163a0cc9e63db628b50775276fad5dd356653c09
double <2637309949@qq.com>
2019-07-05 16:59:59 +0800

    style: template

--- dist/apidoc/template/api_project.js
+++ dist/apidoc/template/api_project.js
@@ -1,12 +1,12 @@
 "use strict";
 
 define({
-  "name": "juglans-template",
-  "version": "0.1.0",
+  "name": "bulrush-template",
+  "version": "1.0",
   "description": "api documentation",
-  "title": "juglans-template - api documentation",
-  "url": "http://127.0.0.1:3001/api/v1",
-  "sampleUrl": "http://127.0.0.1:3001/api/v1",
+  "title": "bulrush-template - api documentation",
+  "url": "http://127.0.0.1:8080/api/v1",
+  "sampleUrl": "http://127.0.0.1:8080/api/v1",
   "template": {
     "forceLanguage": "zh_cn",
     "withCompare": false,
@@ -16,8 +16,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-05T08:08:21.204Z",
+    "time": "2019-06-13T06:44:04.098Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.7"
+    "version": "0.17.6"
   }
 });
