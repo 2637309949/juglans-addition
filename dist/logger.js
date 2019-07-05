77dfce88266770bc75b10da3a58fd78f5355e6f9
double <2637309949@qq.com>
2019-07-05 16:01:30 +0800

    feat: loger通道化，统一化

--- dist/logger.js
+++ dist/logger.js
@@ -3,17 +3,8 @@
 // Copyright (c) 2018-2020 Double.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
-const fsx = require('fs-extra');
-
-const path = require('path');
-
 const winston = require('winston');
 
-let transports;
-let {
-  JLOG_PATH = '',
-  JLOG_SIZE = 10
-} = process.env;
 let {
   combine,
   timestamp,
@@ -27,32 +18,14 @@ const format = combine(colorize(), timestamp(), printf((_ref) => {
     timestamp
   } = _ref;
   return `[${level}]: ${timestamp} ${message}`;
-}));
-
-if (JLOG_PATH) {
-  fsx.ensureDirSync(JLOG_PATH);
-  JLOG_SIZE = parseInt(JLOG_SIZE);
-  transports = [new winston.transports.File({
-    filename: path.join(JLOG_PATH, 'error.log'),
-    level: 'error',
-    maxsize: 1024 * JLOG_SIZE
-  }), new winston.transports.File({
-    filename: path.join(JLOG_PATH, 'combined.log'),
-    maxsize: 1024 * JLOG_SIZE
-  }), new winston.transports.Console({
-    format
-  })];
-} else {
-  transports = [new winston.transports.Console({
-    format
-  })];
-} // just for juglans log
-
+})); // just for juglans log
 
 module.exports = winston.createLogger({
   level: 'info',
   format,
-  transports
+  transports: [new winston.transports.Console({
+    format
+  })]
 }); // create your project log by this ones
 
 module.exports.winston = winston;
